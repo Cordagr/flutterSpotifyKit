217d036fefbcff62245c7aa3cd8b10349ddcb895
Cordagr <113870656+Cordagr@users.noreply.github.com>
2025-01-29 18:44:25 -0600

    Update requestHandler.java
    Updating request handler

--- requestHandler.java
+++ requestHandler.java
@@ -1,27 +1,60 @@
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
-import androidx.appcompat.app.AppCompatActivity;
+public class SpotifyAuthService {
 
-public class SpotifyCallbackActivity extends AppCompatActivity {
+    private static final String TOKEN_URL = "https://accounts.spotify.com/api/token";
+    private static final String CLIENT_ID = "YOUR_CLIENT_ID";
+    private static final String CLIENT_SECRET = "YOUR_CLIENT_SECRET";
+    private static final String REDIRECT_URI = "http://localhost:8888/callback";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void exchangeCodeForToken(String code) {
+        OkHttpClient client = new OkHttpClient();
 
-        Uri uri = getIntent().getData();
-        if (uri != null && uri.toString().startsWith("http://localhost:8888/callback")) {
-            String code = uri.getQueryParameter("code");
-            if (code != null) {
-                // Exchange the authorization code for an access token
-                exchangeCodeForToken(code);
+        RequestBody formBody = new FormBody.Builder()
+                .add("grant_type", "authorization_code")
+                .add("code", code)
+                .add("redirect_uri", REDIRECT_URI)
+                .add("client_id", CLIENT_ID)
+                .add("client_secret", CLIENT_SECRET)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(TOKEN_URL)
+                .post(formBody)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                // Handle the error here
             }
-        }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String responseBody = response.body().string();
+                    JsonObject json = JsonParser.parseString(responseBody).getAsJsonObject();
+                    String accessToken = json.get("access_token").getAsString();
+                    String refreshToken = json.get("refresh_token").getAsString();
+                    int expiresIn = json.get("expires_in").getAsInt();
+
+                    // Store the access token and refresh token securely
+                    storeAccessToken(accessToken, refreshToken, expiresIn);
+                } else {
+                    // Handle the error here
+                    System.err.println("Error: " + response.code() + " - " + response.message());
+                }
+            }
+        });
     }
 
-    private void exchangeCodeForToken(String code) {
-        // Implement the logic to exchange the code for an access token
-        // This might involve making a network request to Spotify's token endpoint
+    private void storeAccessToken(String accessToken, String refreshToken, int expiresIn) {
+        // Implement secure storage of the access token and refresh token
+        // This could involve SharedPreferences, a database, or encrypted storage
+        System.out.println("Access Token: " + accessToken);
+        System.out.println("Refresh Token: " + refreshToken);
+        System.out.println("Expires In: " + expiresIn);
     }
 }
